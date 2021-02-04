a6f5a949fdebbd1960bbb961f94cfb1c161865dc
antoniapuspan <65085166+antoniapuspan@users.noreply.github.com>
2021-02-04 16:57:23 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+start();
+
+function start(); {
+    console.log("start");
+    getUserSelection();
+}
+
+function getUserSelection(); {
+    makeRandomComputerChoice();
+}
+
+function makeRandomComputerChoice(mComputerChoice(); {
+        showAnimations();
+    }
+
+    function showAnimations(); {
+        determineWinner();
+    }
+    let winner = "";
+
+    function determineWinner() {
+        // FAKED so player always wins!
+        winner = "player"
+
+        // if player won!
+        showWin();
+
+    }
+
+    function showWin() {
+        console.log("Show win!");
+        //show winner!
+    }
+
+    function showLose() {
+
+    }
+
+    function showDraw() {
+
+    }
